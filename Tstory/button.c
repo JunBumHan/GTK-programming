6dc3b1d6a117ad1ec7e696cb4e9113aa01109928
JunBumHan <2hanjunbum6@gmail.com>
2022-09-21 22:20:42 +0900

    [GTK]

--- Tstory/button.c
+++ Tstory/button.c
@@ -12,7 +12,7 @@ static void activate(GtkApplication* app, gpointer user_data)
 
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Window");
-    gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+    gtk_window_set_default_size(GTK_WINDOW(window),1000, 700);
     
     button = gtk_button_new_with_label("Hello, world");
     /*
